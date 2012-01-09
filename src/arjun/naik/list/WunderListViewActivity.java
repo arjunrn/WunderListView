070877ee192ebb19254ebd6ce830e8b59ac444a0
Arjun Naik <arjunnaik@Arjuns-Mac-Pro.local>
2012-01-10 01:43:44 +0530

    First commit with empty project

---
+++ src/arjun/naik/list/WunderListViewActivity.java
@@ -0,0 +1,13 @@
+package arjun.naik.list;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WunderListViewActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
